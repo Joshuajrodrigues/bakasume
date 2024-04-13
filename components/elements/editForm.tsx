f474c97f01879f70f9853b30079a57804636add9
joshuajrodrigues <rodrigues.joshr@gmail.com>
2024-04-13 21:10:44 +0530

    contact info added

--- components/elements/editForm.tsx
+++ components/elements/editForm.tsx
@@ -1,7 +1,7 @@
-"use client"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, useFieldArray } from "react-hook-form";
+import { z } from "zod";
 import {
     Form,
     FormControl,
@@ -10,38 +10,49 @@ import {
     FormItem,
     FormLabel,
     FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 const userContactInfo = z.object({
-    info: z.string()
-})
+    value: z.string(),
+});
 
 const formSchema = z.object({
     name: z.string().min(2, {
         message: "Name must be at least 2 characters.",
     }),
     contact: z.array(userContactInfo).min(1, {
-        message: "Add some info on how to contact you"
-    })
-
-})
+        message: "Add some info on how to contact you",
+    }),
+});
 
 const EditForm = () => {
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
             name: "",
+            contact: [{ value: "" }]
         },
-    })
+    });
+
+    const {
+        fields: contactFields,
+        append: addContactFields,
+        remove: removeContactFields,
+    } = useFieldArray({
+        name: "contact",
+        control: form.control,
+    });
+
     function onSubmit(values: z.infer<typeof formSchema>) {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
-        console.log(values)
+        console.log(values);
     }
     return (
-        <div>
+        <div className="mt-4">
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                     <FormField
@@ -60,28 +71,37 @@ const EditForm = () => {
                             </FormItem>
                         )}
                     />
-                    <FormField
-                        control={form.control}
-                        name="contact"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Contact Info</FormLabel>
-                                <FormControl>
+                    {
+                        contactFields.map((field, index) => (
 
-                                </FormControl>
-                                <FormDescription>
-                                    Add contact information like phone, email etc.
-                                </FormDescription>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
+                            <FormField
 
-                    <Button type="submit">Save & Preview</Button>
+                                key={field.id}
+                                control={form.control}
+                                name={`contact.${index}.value`}
+                                render={({ field }) => (
+                                    <FormItem className={cn(index !== 0 && "-space-y-4")}>
+                                        <FormLabel className={cn(index !== 0 && "sr-only")}>Contact Info</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="" {...field} />
+                                        </FormControl>
+                                        <FormDescription className={cn(index !== 0 && "sr-only")}>
+                                            Add contact information like phone, email etc.
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        ))
+                    }
+                    <Button type="button" variant={"outline"} size={"sm"} onClick={() => addContactFields({ value: "" })}>Add more</Button>
+
+
+                    <Button className="block" type="submit">Save & Preview</Button>
                 </form>
             </Form>
         </div>
-    )
-}
+    );
+};
 
-export default EditForm
+export default EditForm;
