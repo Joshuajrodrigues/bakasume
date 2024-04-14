531b2b33fb5d8232816eaab60d409b95d3cfd93d
joshuajrodrigues <rodrigues.joshr@gmail.com>
2024-04-14 21:06:48 +0530

    code refactor

--- components/elements/resumePreview.tsx
+++ components/elements/resumePreview.tsx
@@ -1,152 +1,29 @@
 "use client"
-import { BlobProvider, Document, Font, Page, StyleSheet, Text } from '@react-pdf/renderer';
-import { Page as DocumentPage, Document as DocumentViewer, pdfjs } from 'react-pdf';
-import { Button } from '../ui/button';
-import { PiDownloadBold } from "react-icons/pi"
+import { BlobProvider } from '@react-pdf/renderer';
 import { useRef, useState } from 'react';
+import { PiDownloadBold } from "react-icons/pi";
+import { Page as DocumentPage, Document as DocumentViewer, pdfjs } from 'react-pdf';
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
+import { Resume } from '../resumeTemplates/default';
+import { Button } from '../ui/button';
 import Spin from './loading';
-
+// https://github.com/diegomura/react-pdf/issues/714
+//https://github.com/wojtekmaj/react-pdf/wiki/Recipes
+//https://github.com/diegomura/react-pdf/issues/1113
+//work shit => https://stackoverflow.com/questions/75253619/react-pdf-viewer-library-cannot-render-the-first-page-as-soon-as-the-minimum-req
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
     "pdfjs-dist/build/pdf.worker.min.js",
     import.meta.url
 ).toString();
 
 
-
-Font.register({
-    family: 'Oswald',
-    src: 'https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf'
-});
-const styles = StyleSheet.create({
-    body: {
-        paddingTop: 35,
-        paddingBottom: 65,
-        paddingHorizontal: 35,
-    },
-    title: {
-        fontSize: 24,
-        textAlign: 'center',
-        fontFamily: 'Oswald'
-    },
-    author: {
-        fontSize: 12,
-        textAlign: 'center',
-        marginBottom: 40,
-    },
-    subtitle: {
-        fontSize: 18,
-        margin: 12,
-        fontFamily: 'Oswald'
-    },
-    text: {
-        margin: 12,
-        fontSize: 14,
-        textAlign: 'justify',
-        fontFamily: 'Times-Roman'
-    },
-    image: {
-        marginVertical: 15,
-        marginHorizontal: 100,
-    },
-    header: {
-        fontSize: 12,
-        marginBottom: 20,
-        textAlign: 'center',
-        color: 'grey',
-    },
-    pageNumber: {
-        position: 'absolute',
-        fontSize: 12,
-        bottom: 30,
-        left: 0,
-        right: 0,
-        textAlign: 'center',
-        color: 'grey',
-    },
-});
-
-const Resume = (
-    <Document >
-        <Page style={styles.body}>
-            <Text style={styles.header} fixed>
-                ~ Created with react-pdf ~
-            </Text>
-            <Text style={styles.title}>Don Quijote de la Mancha</Text>
-            <Text style={styles.author}>Miguel de Cervantes</Text>
-            <Text style={styles.subtitle}>
-                Capítulo I: Que trata de la condición y ejercicio del famoso hidalgo D.
-                Quijote de la Mancha
-            </Text>
-            <Text style={styles.text}>
-                En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha
-                mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga
-                antigua, rocín flaco y galgo corredor. Una olla de algo más vaca que
-                carnero, salpicón las más noches, duelos y quebrantos los sábados,
-                lentejas los viernes, algún palomino de añadidura los domingos,
-                consumían las tres partes de su hacienda. El resto della concluían sayo
-                de velarte, calzas de velludo para las fiestas con sus pantuflos de lo
-                mismo, los días de entre semana se honraba con su vellori de lo más
-                fino. Tenía en su casa una ama que pasaba de los cuarenta, y una sobrina
-                que no llegaba a los veinte, y un mozo de campo y plaza, que así
-                ensillaba el rocín como tomaba la podadera. Frisaba la edad de nuestro
-                hidalgo con los cincuenta años, era de complexión recia, seco de carnes,
-                enjuto de rostro; gran madrugador y amigo de la caza. Quieren decir que
-                tenía el sobrenombre de Quijada o Quesada (que en esto hay alguna
-                diferencia en los autores que deste caso escriben), aunque por
-                conjeturas verosímiles se deja entender que se llama Quijana; pero esto
-                importa poco a nuestro cuento; basta que en la narración dél no se salga
-                un punto de la verdad
-            </Text>
-            <Text style={styles.text}>
-                Es, pues, de saber, que este sobredicho hidalgo, los ratos que estaba
-                ocioso (que eran los más del año) se daba a leer libros de caballerías
-                con tanta afición y gusto, que olvidó casi de todo punto el ejercicio de
-                la caza, y aun la administración de su hacienda; y llegó a tanto su
-                curiosidad y desatino en esto, que vendió muchas hanegas de tierra de
-                sembradura, para comprar libros de caballerías en que leer; y así llevó
-                a su casa todos cuantos pudo haber dellos; y de todos ningunos le
-                parecían tan bien como los que compuso el famoso Feliciano de Silva:
-                porque la claridad de su prosa, y aquellas intrincadas razones suyas, le
-                parecían de perlas; y más cuando llegaba a leer aquellos requiebros y
-                cartas de desafío, donde en muchas partes hallaba escrito: la razón de
-                la sinrazón que a mi razón se hace, de tal manera mi razón enflaquece,
-                que con razón me quejo de la vuestra fermosura, y también cuando leía:
-                los altos cielos que de vuestra divinidad divinamente con las estrellas
-                se fortifican, y os hacen merecedora del merecimiento que merece la
-                vuestra grandeza.
-            </Text>
-            <Text style={styles.text}>
-                Es, pues, de saber, que este sobredicho hidalgo, los ratos que estaba
-                ocioso (que eran los más del año) se daba a leer libros de caballerías
-                con tanta afición y gusto, que olvidó casi de todo punto el ejercicio de
-                la caza, y aun la administración de su hacienda; y llegó a tanto su
-                curiosidad y desatino en esto, que vendió muchas hanegas de tierra de
-                sembradura, para comprar libros de caballerías en que leer; y así llevó
-                a su casa todos cuantos pudo haber dellos; y de todos ningunos le
-                parecían tan bien como los que compuso el famoso Feliciano de Silva:
-                porque la claridad de su prosa, y aquellas intrincadas razones suyas, le
-                parecían de perlas; y más cuando llegaba a leer aquellos requiebros y
-                cartas de desafío, donde en muchas partes hallaba escrito: la razón de
-                la sinrazón que a mi razón se hace, de tal manera mi razón enflaquece,
-                que con razón me quejo de la vuestra fermosura, y también cuando leía:
-                los altos cielos que de vuestra divinidad divinamente con las estrellas
-                se fortifican, y os hacen merecedora del merecimiento que merece la
-                vuestra grandeza.
-            </Text>
-            <Text style={styles.pageNumber} render={({ pageNumber, totalPages }) => (
-                `${pageNumber} / ${totalPages}`
-            )} fixed />
-        </Page>
-    </Document>
-)
-
-
 const ResumePreview = () => {
     const [numPages, setNumPages] = useState<number>(1);
     const [pageNumber, setPageNumber] = useState<number>(1)
     const [isPageReady, setIsPageReady] = useState(false)
+    const parentRef = useRef<HTMLDivElement>(null);
+
     function changePage(offset: number) {
         setPageNumber(prevPageNumber => prevPageNumber + offset);
     }
@@ -163,9 +40,6 @@ const ResumePreview = () => {
         setNumPages(numPages);
         setPageNumber(1);
     }
-    const parentRef = useRef<HTMLDivElement>(null);
-
-
 
     return (
         <div ref={parentRef} className='flex flex-col justify-center items-center'>
@@ -181,7 +55,6 @@ const ResumePreview = () => {
                                 loading={loading ? <Spin />
                                     : null}
                             >
-
                                 <DocumentPage
                                     loading={loading ? <Spin /> : null}
                                     pageNumber={pageNumber}
@@ -193,8 +66,6 @@ const ResumePreview = () => {
                             {
                                 isPageReady &&
                                 <div className='flex space-x-2'>
-
-
                                     <Button
                                         type="button"
                                         variant={"outline"}
@@ -215,7 +86,6 @@ const ResumePreview = () => {
                                     >
                                         Next
                                     </Button>
-
                                 </div>
                             }
                         </>
--- next.config.mjs
+++ next.config.mjs
@@ -12,13 +12,7 @@ const nextConfig = {
     },
     webpack: (config) => {
         config.resolve.alias.canvas = false;
-        config.module.rules.push({
-            test: /pdfjs-dist\/build\/pdf\.worker\.js$/,
-            type: "asset/resource",
-            generator: {
-                filename: "static/chunks/[name].[hash][ext]",
-            },
-        });
+
         return config;
     },
 };
