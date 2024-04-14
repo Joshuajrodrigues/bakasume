dcba349ad01852c297674b87fae432e39c3307d7
joshuajrodrigues <rodrigues.joshr@gmail.com>
2024-04-14 20:48:53 +0530

    top margin fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { TypographyH1 } from "@/components/typography/typographyH1";
 
 export default function Home() {
   return (
-    <main className="m-2 flex flex-col justify-start items-center w-max-full min-h-svh">
-      <TypographyH1>
+    <main className="m-2 mt-12 flex flex-col justify-start items-center w-max-full min-h-svh">
+      <TypographyH1 >
         BAKASUME
       </TypographyH1>
       <AppSwitch />
